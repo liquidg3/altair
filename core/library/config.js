fb5e2d665490b262ba05d0ef5c220de0a36a1f7d
Hemstreet <jonhemstreet@gmail.com>
2014-01-08 21:51:56 -0700

    Git ignore w/ .idea and .idea/workspace.xml

--- core/library/config.js
+++ core/library/config.js
@@ -1,18 +1,19 @@
-define("altair/config", null, function (dojo) {
+define("altair/config", function (dojo) {
     return {
         load: function(id, require, load){
            console.log('load: ', id);
         },
         normalize: function(id, toAbsMid){
 
+            console.log(id);
             var path = id.split('.json')[0];
 
             console.log(dojo);
 
-            return '';
+//            return '';
 
 
-            return 'poop';
+//            return 'poop';
             console.log('normalize', arguments);
             return id;
         }};
